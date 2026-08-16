832b75018e55febc5c29bd19c5050fac82d1ad5f
Soltani Ghaith <ghaithsoltani36@gmail.com>
2026-08-16 08:48:41 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,23 @@
 def buildJar() {
     echo 'building the application...'
-    sh 'mvn package'
+    sh 'mvn clean package'
+}
+def incrementVersion() {
+    echo 'incrementing app version....'
+    sh 'mvn build-helper:parse-version versions:set \
+                  -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementVersion} \
+                  versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.*)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "version-$BUILD_NUMBER"
 }
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t ghaithsoltani/demo-app:jma-2.0 .'
+        sh 'docker build -t ghaithsoltani/demo-app:${IMAGE_NAME} .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push ghaithsoltani/demo-app:jma-2.0'
+        sh 'docker push ghaithsoltani/demo-app:${IMAGE_NAME}'
     }
 }
 
